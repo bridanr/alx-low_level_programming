5241d3bc52e9027bdc6e6e61f4ba453b2bd9481e
Romeo Asamoah <romeoasamoah08@gmail.com>
2024-06-26 00:02:57 -0700

    f betty

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
-/**  
+/**
  * _islower - Checks for alphabetic character
  * @c: The har to be checked
  * Return: 1 for alphabetic character or 0 for anything else
  */
 
 int _islower(int c)
-{	
-	if (c >= 65 && c <= 97)
-       		return (1);
-       	else
-		return (0);
-}                           
+{
+if (c >= 65 && c <= 97)
+return (1);
+else
+return (0);
+}
