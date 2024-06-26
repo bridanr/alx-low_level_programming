4775b2e5e72d9bb3c3fb23ab40d681d95e9e0960
Romeo Asamoah <romeoasamoah08@gmail.com>
2024-06-26 12:15:12 -0700

    sec chance

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * _islower - Checks for alphabetic character
+ * _islower - Checks for lowercase character
  * @c: The har to be checked
- * Return: 1 for alphabetic character or 0 for anything else
+ * Return: 1 for lowercase character or 0 for anything else
  */
 
 int _islower(int c)
 {
-if (c >= 65 && c <= 97)
-return (1);
-else
-return (0);
+	if (c >= 65 && c <= 97)
+	{
+	return (1);
+	}
+	return (0);
 }
