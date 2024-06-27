712f17ff8a5db75cc8a33adc47c21ea842ec3cfc
Romeo Asamoah <romeoasamoah08@gmail.com>
2024-06-27 13:51:30 -0700

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,14 +12,12 @@ void print_diagonal(int n)
 	for (i = 0 ; i < n ; i++)
 	{
 		if (n > 1)
-		{
 			for (j = 0 ; j < i ; j++)
 			{
 				_putchar(' ');
 			}
 			_putchar('\\');
 			_putchar('\n');
-		}
 	}
 	if (n > 0)
 	{
