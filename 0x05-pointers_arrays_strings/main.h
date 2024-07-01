c4ad94c645327149c81ef9ee6325c60d380fb25b
Romeo Asamoah <romeoasamoah08@gmail.com>
2024-07-01 23:24:28 +0000

    task 0-complete

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,9 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
+int _putchar(char c);
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
 
 #endif
