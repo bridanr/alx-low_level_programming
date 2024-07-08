69fc0818e28f340b852f3e38855174656a6251e8
Romeo Asamoah <romeoasamoah08@gmail.com>
2024-07-08 23:47:29 +0000

    final commit

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define _MAIN_H_
 #include <stdio.h>
 
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
